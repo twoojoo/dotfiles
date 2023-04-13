772018a1e48b835894e199587748ae5dd094d46b
Giovanni Campeol <giovanni@smartpricing.it>
2023-04-13 11:23:41 +0200

    updates

--- i3/scripts/tidy.sh
+++ i3/scripts/tidy.sh
@@ -1,6 +1,7 @@
-i3-msg '[class=.*] move container to workspace 6' &&
-i3-msg '[class=Chromium] move container to workspace 1' &&
-i3-msg '[class=Subl] move container to workspace 2' &&
-i3-msg '[class=beekeeper-studio] move container to workspace 3' &&
-i3-msg '[class=Insomnia] move container to workspace 4' &&
-i3-msg '[class=Terminator] move container to workspace 5' 
+i3-msg '[class=.*] move container to workspace 7' &&
+i3-msg '[class=Chromium] move container to workspace 1' &
+i3-msg '[class=Subl] move container to workspace 2' &
+i3-msg '[class=beekeeper-studio] move container to workspace 3' &
+i3-msg '[class=Insomnia] move container to workspace 4' &
+i3-msg '[class=Lens] move container to workspace 5' &
+i3-msg '[class=Terminator] move container to workspace 6' 
