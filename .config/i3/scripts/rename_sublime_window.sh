b2045e388666123dc1ac44c6be66a0d78f33a0f0
Giovanni Campeol <giovanni@smartpricing.it>
2023-05-10 08:38:14 +0200

    tidy script updates

--- .config/i3/scripts/rename_sublime_window.sh
+++
@@ -1,5 +0,0 @@
-directory_name=$(echo $1 | rev | cut -d "/" -f 1 | rev)
-echo $directory_name
-sublime_win_name="untitled • ($directory_name) - Sublime Text (UNREGISTERED)"
-echo $sublime_win_name
-xdotool search --name "untitled • ($directory_name) - Sublime Text (UNREGISTERED)" set_window --name "Sublime Text - $directory_name"
--- .config/i3/scripts/tidy.sh
+++ .config/i3/scripts/tidy.sh
@@ -4,8 +4,8 @@
 BROWSERS_WS=1
 EDITORS_WS=2
 DATABASE_WS=3
-HTTP_WS=4
-K8S_WS=5
+K8S_WS=4
+HTTP_WS=5
 TERMINAL_WS=6
 EVERYTHING_ELSE_WS=7
 
@@ -19,6 +19,7 @@ i3-msg "[class=Firefox] move container to workspace $BROWSERS_WS" &> /dev/null
 ## Text editors
 i3-msg "[class=Subl] move container to workspace $EDITORS_WS" &> /dev/null
 i3-msg "[class=code-oss] move container to workspace $EDITORS_WS" &> /dev/null
+i3-msg "[class=kate] move container to workspace $EDITORS_WS" &> /dev/null
 
 ## Database clients
 i3-msg "[class=beekeeper-studio] move container to workspace $DATABASE_WS" &> /dev/null
@@ -34,3 +35,4 @@ i3-msg "[class=Lens] move container to workspace $K8S_WS" &> /dev/null
 ## Terminals
 i3-msg "[class=Terminator] move container to workspace $TERMINAL_WS" &> /dev/null
 i3-msg "[class=konsole] move container to workspace $TERMINAL_WS" &> /dev/null
+i3-msg "[class=Alacritty] move container to workspace $TERMINAL_WS" &> /dev/null
